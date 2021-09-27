25e078eb075056e198e4eb1ed6ae074bd01cf172
Bachir Seghir <bachir.seghir17@gmail.com>
2021-09-27 21:44:39 +0100

    modified test.js

--- src/test.js
+++ src/test.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function test() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="test">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default test;
